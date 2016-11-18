d846cc4097276a53cc6e19dc86cbbd68ae50d571
kpetrone1 <kpetrone1@babson.edu>
2016-11-17 20:01:12 -0500

    Update 11/17

--- mbta_helper.py
+++ mbta_helper.py
@@ -11,14 +11,6 @@ MBTA_DEMO_API_KEY = "wX9NwuHnZU2ToO7GmGR9uw"
 
 url = "https://maps.googleapis.com/maps/api/geocode/json?address=231+Forest+Street,+Wellesley,+MA+02457,+USA&key=AIzaSyDA1865acKD_jP_65hIQ24lo1LmUjHiRHY"
 
-f = urllib.request.urlopen(url)
-response_text = f.read().decode('utf-8')
-response_data = json.loads(response_text)
-
-pprint(response_data)
-print('\n')
-print("Address: ")
-print(response_data["results"][0]["formatted_address"])
 
 # A little bit of scaffolding if you want to use it
 
@@ -27,11 +19,16 @@ def get_json(url):
     Given a properly formatted URL for a JSON web API request, return
     a Python JSON object containing the response to that request.
     """
+
+    """
+    Set "f", the file, equal to the information from the URL. Read the
+    file and return the information in a nicely-formatted JSON display.
+    """
+
     f = urllib.request.urlopen(url)
     response_text = f.read().decode('utf-8')
     response_data = json.loads(response_text)
     return response_data
-
 
 def get_lat_long(place_name):
     """
@@ -40,12 +37,30 @@ def get_lat_long(place_name):
     See https://developers.google.com/maps/documentation/geocoding/
     for Google Maps Geocode API URL formatting requirements.
     """
+
+
+    """
+    If a space is entered in the input, replace the space with "%20"
+    so that the input can be properly added to the URL. Return the
+    latitude and longitude from the outputted results generated from
+    the "get_json" function.
+    """
+
+    if ' ' in place_name:
+        while ' ' in place_name:
+            x = []
+            for i in place_name:
+                x.append(i)
+            while ' ' in x:
+                x[x.index(' ')] = '%20'
+            place_name = ''.join(x)
+
     url = GMAPS_BASE_URL + '?address=' + place_name
     place_json = get_json(url)
     lat = place_json['results'][0]['geometry']['location']['lat']
     lon = place_json['results'][0]['geometry']['location']['lng']
     return (lat, lon)
-
+        
 
 def get_nearest_station(latitude, longitude):
     """
@@ -54,24 +69,35 @@ def get_nearest_station(latitude, longitude):
     See http://realtime.mbta.com/Portal/Home/Documents for URL
     formatting requirements for the 'stopsbylocation' API.
     """
-    pass
+    MBTA_url = MBTA_BASE_URL + '?api_key=wX9NwuHnZU2ToO7GmGR9uw&lat='+str(latitude)+'&lon='+str(longitude)+'&format=json'
+    response_data_mbta = get_json(MBTA_url)
 
+    if response_data_mbta['stop'] != []:
+        x = 0
+        station_name = response_data_mbta['stop'][x]['parent_station_name']
+        distance = response_data_mbta['stop'][x]['distance']
+        while station_name == '':
+            x += 1
+            station_name = response_data_mbta['stop'][x]['parent_station_name']
+            distance = response_data_mbta['stop'][x]['distance']
+        return station_name, distance
 
 def find_stop_near(place_name):
     """
     Given a place name or address, return the nearest MBTA stop and the 
     distance from the given place to that stop.
     """
-    pass
+    lat, lon = get_lat_long(place_name)
+    return get_nearest_station(lat, lon)
+
 
 
 def main():
     place = input()
-    # url = GMAPS_BASE_URL + '?address=' + place
-    # pprint(get_json(url))
 
-    # print(find_stop_near(place_name))
-    print(get_lat_long(place))
+    print(find_stop_near(place))
+
+
 if __name__ == '__main__':
     main()
-    
+   
